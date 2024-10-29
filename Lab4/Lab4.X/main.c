9189672de023c8ec36640e6ba1bec7a2a4eb5675
heimsnik <cmheimvik@gmail.com>
2024-10-29 21:28:47 +0100

    Finished task 4

--- Lab4/Lab4.X/main.c
+++ Lab4/Lab4.X/main.c
@@ -8,30 +8,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <avr/io.h>
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 #include "ac.h"
 #include "led.h"
 
 
+ISR(TCA0_OVF_vect)
+{
+    //Check the light in the room
+    if(readAC() & 0x10){
+        setLed();
+    } else {
+        clearLed();
+    }
+    
+    // Clear interrupt flag
+    TCA0.SINGLE.INTFLAGS = TCA_SINGLE_OVF_bm;
+}
+ISR(AC0_AC_vect)
+{
+    //Check the light in the room
+    if(readAC() & 0x10){
+        setLed();
+    } else {
+        clearLed();
+    }
+    
+    // Clear interrupt flag
+    AC0.STATUS |= (0x1<<AC_CMPIF_bp);
+}
+
 
 
 int main(void) {
-    initAC(0);
-    initLed();
     USART3_Init();
+    initAC(1);
+    initLed();
+    
 
-    USART3_SendString("All regs:\n");
-    printACRegisters(); 
-    // Main loop
-    while (1) {
-        USART3_SendHex(AC0.STATUS,"Status: ");
-        
-        if(readAC() & 0x10){
-            setLed();
-        } else {
-            USART3_SendString("CLR\n");
-            clearLed();
-        }
-        _delay_ms(100);
+    //initTC(100);
+    //Enable global interrupts
+    sei();
+    //Setup interrupt controller
+    set_sleep_mode(SLEEP_MODE_STANDBY);
+    while(1) {
+        sleep_mode();
     }
+    return 0;
 }
